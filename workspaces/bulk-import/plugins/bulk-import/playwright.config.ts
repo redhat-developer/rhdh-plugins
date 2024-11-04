78f599728623051b12ba7a184dc0fdd084d30590
Kashish Mittal <kmittal@redhat.com>
2024-11-04 14:19:40 -0500

    fix prettier issues
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>

--- workspaces/bulk-import/plugins/bulk-import/playwright.config.ts
+++ workspaces/bulk-import/plugins/bulk-import/playwright.config.ts
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */import { defineConfig, devices } from '@playwright/test';
+ */ import { defineConfig, devices } from '@playwright/test';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
