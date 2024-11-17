6d1f8daaeafa55ee3392b945f8e36a1fd982aafa
Kashish Mittal <kmittal@redhat.com>
2024-11-17 00:30:33 -0500

    fix prettier issues
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>

--- workspaces/bulk-import/plugins/bulk-import/src/components/BulkImportSidebarItem.tsx
+++ workspaces/bulk-import/plugins/bulk-import/src/components/BulkImportSidebarItem.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- import React from 'react';
+import React from 'react';
 
 import { SidebarItem } from '@backstage/core-components';
 import { configApiRef, useApi } from '@backstage/core-plugin-api';
