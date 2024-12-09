5b90f965e4508667bdfd628800cd04821cff73f2
Bat-Zion Rotman <brotman@redhat.com>
2024-12-09 18:00:14 +0200

    chore(orchestrator): resolve dependencies issue (#149)

--- workspaces/orchestrator/plugins/orchestrator-backend/__fixtures__/mockSpringBootWorkflowData.ts
+++ workspaces/orchestrator/plugins/orchestrator-backend/__fixtures__/mockSpringBootWorkflowData.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { JSONSchema7 } from 'json-schema';
+import type { JSONSchema7 } from 'json-schema';
 
 import { WorkflowDefinition } from '@red-hat-developer-hub/backstage-plugin-orchestrator-common';
 
