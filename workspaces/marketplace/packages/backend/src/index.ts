1de876e34ad6c18f29e6b14af993f86378f93efa
Karthik <karthik.jk11@gmail.com>
2024-12-13 13:11:56 +0530

    remove marketplace backend plugin

--- workspaces/marketplace/packages/backend/src/index.ts
+++ workspaces/marketplace/packages/backend/src/index.ts
@@ -59,11 +59,7 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
 
-// marketplace
-backend.add(
-  import('@red-hat-developer-hub/backstage-plugin-marketplace-backend'),
-);
-
+// catalog-backend-module-marketplace
 backend.add(
   import(
     '@red-hat-developer-hub/backstage-plugin-catalog-backend-module-marketplace'
--- workspaces/marketplace/plugins/marketplace-backend/knip-report.md
+++
@@ -1,8 +0,0 @@
-# Knip report
-
-## Unused dependencies (2)
-
-| Name                                                       | Location     | Severity |
-| :--------------------------------------------------------- | :----------- | :------- |
-| @red-hat-developer-hub/backstage-plugin-marketplace-common | package.json | error    |
-| @backstage/catalog-client                                  | package.json | error    |
