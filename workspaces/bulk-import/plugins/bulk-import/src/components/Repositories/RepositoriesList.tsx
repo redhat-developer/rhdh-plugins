ba86c465784ffcffcbc3a20375128dcf2eefe1f8
its-mitesh-kumar <itsmiteshkumar98@gmail.com>
2024-12-13 18:42:47 +0530

    feat(bul-import): fixing typescript error

--- workspaces/bulk-import/plugins/bulk-import/src/components/Repositories/RepositoriesList.tsx
+++ workspaces/bulk-import/plugins/bulk-import/src/components/Repositories/RepositoriesList.tsx
@@ -28,7 +28,6 @@ import {
   AddRepositoryData,
   SortingOrderEnum,
 } from '../../types';
-import { getComparator } from '../../utils/repository-utils';
 import { RepositoriesHeader } from '../AddRepositories/RepositoriesHeader';
 import { AddedRepositoriesTableBody } from './AddedRepositoriesTableBody';
 import DeleteRepositoryDialog from './DeleteRepositoryDialog';
