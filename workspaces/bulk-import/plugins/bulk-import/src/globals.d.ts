ebdeff982b4d55bf7181919146f0acf9eceb3697
Marek Libra <marek.libra@gmail.com>
2024-12-19 15:27:08 +0100

    call eslint fix

--- workspaces/bulk-import/plugins/bulk-import/src/globals.d.ts
+++ workspaces/bulk-import/plugins/bulk-import/src/globals.d.ts
@@ -12,7 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ declare module '*.svg' {
+ */
+declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
