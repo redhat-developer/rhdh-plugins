145e68f444391223d5496bc5287891d97a7d3a68
Lior Soffer <lsoffer@lsoffer-thinkpadx1carbongen12.raanaii.csb>
2025-01-02 13:47:06 +0200

    fix(orchestrator): Implement inputs card in workflow instance page
    Signed-off-by: Lior Soffer <lsoffer@lsoffer-thinkpadx1carbongen12.raanaii.csb>

--- workspaces/orchestrator/plugins/orchestrator/src/components/WorkflowInstancePageContent.tsx
+++ workspaces/orchestrator/plugins/orchestrator/src/components/WorkflowInstancePageContent.tsx
@@ -14,23 +14,30 @@
  * limitations under the License.
  */
 import React from 'react';
+import { useAsync } from 'react-use';
 
-import { Content, InfoCard } from '@backstage/core-components';
+import {
+  Content,
+  InfoCard,
+  StructuredMetadataTable,
+} from '@backstage/core-components';
+import { useApi } from '@backstage/core-plugin-api';
 
 import { Grid, makeStyles } from '@material-ui/core';
 import moment from 'moment';
 
 import {
   AssessedProcessInstanceDTO,
+  InputSchemaResponseDTO,
   ProcessInstanceDTO,
 } from '@red-hat-developer-hub/backstage-plugin-orchestrator-common';
 
+import { orchestratorApiRef } from '../../src/api/api';
 import { VALUE_UNAVAILABLE } from '../constants';
 import { WorkflowProgress } from './WorkflowProgress';
 import { WorkflowResult } from './WorkflowResult';
 import { WorkflowRunDetail } from './WorkflowRunDetail';
 import { WorkflowRunDetails } from './WorkflowRunDetails';
-import { WorkflowVariablesViewer } from './WorkflowVariablesViewer';
 
 export const mapProcessInstanceToDetails = (
   instance: ProcessInstanceDTO,
@@ -77,6 +84,7 @@ export const WorkflowInstancePageContent: React.FC<{
   assessedInstance: AssessedProcessInstanceDTO;
 }> = ({ assessedInstance }) => {
   const styles = useStyles();
+  const orchestratorApi = useApi(orchestratorApiRef);
 
   const details = React.useMemo(
     () => mapProcessInstanceToDetails(assessedInstance.instance),
@@ -94,6 +102,16 @@ export const WorkflowInstancePageContent: React.FC<{
       delete instanceVariables.result;
     }
   }
+  const workflowId = assessedInstance.instance.processId;
+  const instanceId = assessedInstance.instance.id;
+  const { value } = useAsync(async (): Promise<InputSchemaResponseDTO> => {
+    const res = await orchestratorApi.getWorkflowDataInputSchema(
+      workflowId,
+      instanceId,
+    );
+    return res.data;
+  }, [orchestratorApi, workflowId]);
+  const inputs = value?.data;
 
   return (
     <Content noPadding>
@@ -120,17 +138,13 @@ export const WorkflowInstancePageContent: React.FC<{
 
         <Grid item xs={6}>
           <InfoCard
-            title="Variables"
+            title="Inputs"
             divider={false}
             className={styles.bottomRowCard}
             cardClassName={styles.autoOverflow}
           >
-            {instanceVariables && (
-              <WorkflowVariablesViewer variables={instanceVariables} />
-            )}
-            {!instanceVariables && (
-              <div>The workflow instance has no variables</div>
-            )}
+            {inputs && <StructuredMetadataTable dense metadata={inputs} />}
+            {!inputs && <div>The workflow instance has no inputs</div>}
           </InfoCard>
         </Grid>
 
