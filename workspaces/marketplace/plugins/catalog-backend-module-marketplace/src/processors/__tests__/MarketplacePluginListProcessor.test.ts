3ca0097dc12d89d606be9c74e5ad36feb2bd6ed6
Marek Libra <marek.libra@gmail.com>
2025-01-08 09:22:18 +0100

    chore: call eslint fix in all workspaces

--- workspaces/marketplace/plugins/catalog-backend-module-marketplace/src/processors/__tests__/MarketplacePluginListProcessor.test.ts
+++ workspaces/marketplace/plugins/catalog-backend-module-marketplace/src/processors/__tests__/MarketplacePluginListProcessor.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 The Backstage Authors
+ * Copyright Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { MarketplacePluginList } from '@red-hat-developer-hub/backstage-plugin-marketplace-common';
 import { MarketplacePluginListProcessor } from '../MarketplacePluginListProcessor';
 
