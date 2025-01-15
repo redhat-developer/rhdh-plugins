6b60da57b7288a7fd5a10ea397350fe5f5def76d
Christoph Jerolimov <jerolimov+git@redhat.com>
2025-01-15 09:05:47 +0100

    fix(global-header): enable dynamic plugin support for global-header-test plugin (#287)
    * fix(global-header): enable dynamic plugin support for global-header-test plugin
    
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>
    
    * fix(global-header): add workaround for the test-plugin for MUI v5 components
    
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>
    
    ---------
    
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>

---
+++ workspaces/global-header/.changeset/gorgeous-carrots-tie.md
@@ -0,0 +1,5 @@
+---
+'@red-hat-developer-hub/backstage-plugin-global-header-test': patch
+---
+
+Add workaround for the test-plugin for MUI v5 components
--- workspaces/global-header/plugins/global-header-test/src/index.ts
+++ workspaces/global-header/plugins/global-header-test/src/index.ts
@@ -13,4 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { unstable_ClassNameGenerator as ClassNameGenerator } from '@mui/material/className';
+
+ClassNameGenerator.configure(componentName => {
+  return componentName.startsWith('v5-')
+    ? componentName
+    : `v5-${componentName}`;
+});
+
 export * from './plugin';
