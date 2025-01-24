fc027b46112668bcff62f6dae49a44ce0b6ed4a5
Yi Cai <yicai@redhat.com>
2025-01-24 18:46:12 -0500

    feat(global-header): add mountpoints for default header components
    Signed-off-by: Yi Cai <yicai@redhat.com>

--- workspaces/global-header/plugins/global-header/dev/index.tsx
+++ workspaces/global-header/plugins/global-header/dev/index.tsx
@@ -23,6 +23,79 @@ import Button from '@mui/material/Button';
 
 import { globalHeaderPlugin, NotificationBanner } from '../src/plugin';
 import { ExampleComponent } from '../src/components/ExampleComponent';
+import { SearchComponent } from '../src/components/SearchComponent/SearchComponent';
+import { CreateDropdown } from '../src/components/HeaderDropdownComponent/CreateDropdown';
+import { HeaderIconButton } from '../src/components/HeaderIconButtonComponent/HeaderIconButton';
+import ProfileDropdown from '../src/components/HeaderDropdownComponent/ProfileDropdown';
+import { ScalprumContext, ScalprumState } from '@scalprum/react-core';
+import { PluginStore } from '@openshift/dynamic-plugin-sdk';
+import {
+  Slot,
+  ComponentType,
+  GlobalHeaderComponentMountPoint,
+} from '../src/types';
+
+const defaultGlobalHeaderComponentsMountPoints: GlobalHeaderComponentMountPoint[] =
+  [
+    {
+      Component: SearchComponent,
+      config: {
+        enabled: true,
+        type: ComponentType.SEARCH,
+        slot: Slot.HEADER_START,
+        priority: 100, // the greater the number, the more to the left it will be
+      },
+    },
+    {
+      Component: CreateDropdown as React.ComponentType,
+      config: {
+        type: ComponentType.DROPDOWN_BUTTON,
+        enabled: true,
+        slot: Slot.HEADER_START,
+        priority: 90,
+        key: 'create',
+      },
+    },
+    {
+      Component: HeaderIconButton as React.ComponentType,
+      config: {
+        type: ComponentType.ICON_BUTTON,
+        enabled: true,
+        slot: Slot.HEADER_START,
+        priority: 80,
+        props: {
+          icon: 'help', // TODO: come up with a new icon solution
+          tooltip: 'Support',
+          to: '/support',
+        },
+      },
+    },
+    {
+      Component: HeaderIconButton as React.ComponentType,
+      config: {
+        type: ComponentType.ICON_BUTTON,
+        enabled: true,
+        slot: Slot.HEADER_START,
+        priority: 70,
+        props: {
+          key: 'notifications',
+          icon: 'group', // TODO: come up with a new icon solution
+          tooltip: 'Notifications',
+          to: '/notifications',
+        },
+      },
+    },
+    {
+      Component: ProfileDropdown as React.ComponentType,
+      config: {
+        type: ComponentType.DROPDOWN_BUTTON,
+        enabled: true,
+        slot: Slot.HEADER_END,
+        priority: 0, // the greater the number, the more to the left it will be
+        key: 'profile',
+      },
+    },
+  ];
 
 const mockSearchApi = new MockSearchApi({
   results: [
@@ -37,12 +110,28 @@ const mockSearchApi = new MockSearchApi({
   ],
 });
 
+const scalprumState: ScalprumState = {
+  initialized: true,
+  api: {
+    dynamicRootConfig: {
+      mountPoints: {
+        'application/header/component':
+          defaultGlobalHeaderComponentsMountPoints,
+      },
+    },
+  },
+  config: {},
+  pluginStore: new PluginStore(),
+};
+
 createDevApp()
   .registerPlugin(globalHeaderPlugin)
   .addPage({
     element: (
       <TestApiProvider apis={[[searchApiRef, mockSearchApi]]}>
-        <ExampleComponent />
+        <ScalprumContext.Provider value={scalprumState}>
+          <ExampleComponent />
+        </ScalprumContext.Provider>
       </TestApiProvider>
     ),
     title: 'Global Header',
