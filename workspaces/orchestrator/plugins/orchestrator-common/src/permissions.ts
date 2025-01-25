f62ad9c6171c184b92330d542e426d883ef799bb
Marek Libra <marek.libra@gmail.com>
2025-01-25 10:48:43 +0200

    fix(orchestrator): list orchestrator.workflow.use in the RBAC UI
    Signed-off-by: Marek Libra <marek.libra@gmail.com>

--- workspaces/orchestrator/plugins/orchestrator-common/src/permissions.ts
+++ workspaces/orchestrator/plugins/orchestrator-common/src/permissions.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 The Backstage Authors
+ * Copyright Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { createPermission } from '@backstage/plugin-permission-common';
 
 export const orchestratorWorkflowPermission = createPermission({
@@ -41,4 +42,7 @@ export const orchestratorWorkflowUseSpecificPermission = (workflowId: string) =>
     attributes: {},
   });
 
-export const orchestratorPermissions = [orchestratorWorkflowPermission];
+export const orchestratorPermissions = [
+  orchestratorWorkflowPermission,
+  orchestratorWorkflowUsePermission,
+];
