b7c2e3b9886c9ba4fd47ed4f080dfac5d492cca2
Christoph Jerolimov <jerolimov+git@redhat.com>
2025-02-11 14:25:29 +0100

    feat(marketplace): add extra page and tab compontent to integrate marketplace into the existing plugins page
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>

---
+++ workspaces/marketplace/plugins/marketplace/src/pages/InternalPluginInfoTab.tsx
@@ -0,0 +1,125 @@
+/*
+ * Copyright The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { Page, Header, TabbedLayout } from '@backstage/core-components';
+
+import { useScalprum } from '@scalprum/react-core';
+
+import { ReactQueryProvider } from '../components/ReactQueryProvider';
+
+import { MarketplaceCatalogContent } from '../components/MarketplaceCatalogContent';
+
+// import { MarketplaceCollectionsGrid } from '../components/MarketplaceCollectionsGrid';
+import { MarketplaceCollectionPage } from './MarketplaceCollectionPage';
+
+// import { MarketplacePluginsTable } from '../components/MarketplacePluginsTable';
+import { MarketplacePluginDrawer } from '../components/MarketplacePluginDrawer';
+import { MarketplacePluginInstallPage } from './MarketplacePluginInstallPage';
+
+// import { MarketplacePackagesTable } from '../components/MarketplacePackagesTable';
+import { MarketplacePackageDrawer } from '../components/MarketplacePackageDrawer';
+import { MarketplacePackageInstallPage } from './MarketplacePackageInstallPage';
+
+export interface PluginTab {
+  Component: React.ComponentType;
+  config: {
+    path: string;
+    title: string;
+  };
+}
+
+export interface ScalprumState {
+  api?: {
+    dynamicRootConfig?: {
+      mountPoints?: {
+        'internal.plugins/tab': PluginTab[];
+      };
+    };
+  };
+}
+
+const Tabs = () => {
+  const scalprum = useScalprum<ScalprumState>();
+
+  const tabs =
+    scalprum.api?.dynamicRootConfig?.mountPoints?.['internal.plugins/tab'] ||
+    [];
+
+  return (
+    <>
+      <Page themeId="marketplace">
+        <Header title="Plugin" />
+        <TabbedLayout>
+          {/* <TabbedLayout.Route path="/catalog" title="Marketplace">
+            <MarketplaceCatalogContent />
+          </TabbedLayout.Route> */}
+
+          {tabs.map(({ Component, config }) => (
+            <TabbedLayout.Route
+              key={config.path}
+              path={config.path}
+              title={config.title}
+            >
+              <Component />
+            </TabbedLayout.Route>
+          ))}
+
+          {/*       
+          <TabbedLayout.Route path="/collections" title="Collections">
+            <MarketplaceCollectionsGrid />
+          </TabbedLayout.Route>
+          <TabbedLayout.Route path="/plugins" title="Plugins">
+            <MarketplacePluginsTable />
+          </TabbedLayout.Route>
+          <TabbedLayout.Route path="/packages" title="Packages">
+            <MarketplacePackagesTable />
+          </TabbedLayout.Route>
+          */}
+        </TabbedLayout>
+      </Page>
+      <Routes>
+        <Route path="/plugins/:name" Component={MarketplacePluginDrawer} />
+        <Route path="/packages/:name" Component={MarketplacePackageDrawer} />
+      </Routes>
+    </>
+  );
+};
+
+export const InternalPluginInfoTab = () => (
+  <ReactQueryProvider>
+    <Routes>
+      <Route path="/collections/:name" Component={MarketplaceCollectionPage} />
+      <Route
+        path="/plugins/:name/install"
+        Component={MarketplacePluginInstallPage}
+      />
+      <Route
+        path="/packages/:name/install"
+        Component={MarketplacePackageInstallPage}
+      />
+      <Route path="/*" Component={Tabs} />
+    </Routes>
+  </ReactQueryProvider>
+);
+
+export const InternalPluginInfoTabContent = () => (
+  <ReactQueryProvider>
+    <MarketplaceCatalogContent />
+  </ReactQueryProvider>
+);
