825fd8dea59c752d94da188e70a44b2aa1bf1c71
Lior Soffer <43133410+LiorSoffer@users.noreply.github.com>
2025-02-17 09:44:44 +0200

    fix(orchestrator):[UI] Empty values section in Results pane should not be rendered (#420)
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>

---
+++ workspaces/orchestrator/.changeset/tough-queens-hope.md
@@ -0,0 +1,5 @@
+---
+'@red-hat-developer-hub/backstage-plugin-orchestrator': patch
+---
+
+[UI] Empty values section in Results pane should not be rendered
--- workspaces/orchestrator/plugins/orchestrator/src/components/WorkflowResult.tsx
+++ workspaces/orchestrator/plugins/orchestrator/src/components/WorkflowResult.tsx
@@ -299,11 +299,13 @@ const WorkflowOutputs = ({
         </Grid>
       )}
 
-      <Grid item md={12} key="__links" className={styles.values}>
-        <AboutField label="Values">
-          <StructuredMetadataTable dense metadata={nonLinks} />
-        </AboutField>
-      </Grid>
+      {nonLinks?.length > 0 && (
+        <Grid item md={12} key="__links" className={styles.values}>
+          <AboutField label="Values">
+            <StructuredMetadataTable dense metadata={nonLinks} />
+          </AboutField>
+        </Grid>
+      )}
     </>
   );
 };
