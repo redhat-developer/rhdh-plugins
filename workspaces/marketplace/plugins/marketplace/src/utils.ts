95a941116a11ad42d297ccd7d1d05cd96220c8a3
debsmita1 <debsmita.santra@gmail.com>
2025-03-19 15:29:23 +0530

    feat(marketplace): apply appConfig examples to the code editor

---
+++ workspaces/marketplace/plugins/marketplace/src/utils.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Pair, parseDocument, Scalar, YAMLSeq, stringify } from 'yaml';
+import { JsonObject } from '@backstage/types';
+
+export const getYamlContent = (content: JsonObject) => {
+  const yamlString = stringify(content);
+  return `\`\`\`yaml\n${yamlString}\n\`\`\``;
+};
+
+export const applyContent = (
+  editorContent: string,
+  newContent: string,
+  packageName: string,
+) => {
+  if (!editorContent) {
+    return null;
+  }
+  const content = parseDocument(editorContent);
+  const plugins = content.get('plugins');
+
+  if (plugins instanceof YAMLSeq && Array.isArray(plugins?.items)) {
+    (plugins?.items || []).forEach((plugin: any) => {
+      if (plugin instanceof Object) {
+        const pluginPackage = plugin.items?.find(
+          (i: Pair<Scalar, Scalar>) =>
+            i.key.value === 'package' && i.value?.value === packageName,
+        );
+
+        if (pluginPackage) {
+          const formatContent = newContent
+            .replace(/^```yaml\s+/, '')
+            .replace(/\s+```$/, '');
+          plugin.set('pluginConfig', formatContent);
+        }
+      }
+    });
+  }
+  return content.toString();
+};
