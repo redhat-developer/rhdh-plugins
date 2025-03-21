a3a3bc524770deffcf1407f0bd6f2b2fbc439cc0
debsmita1 <debsmita.santra@gmail.com>
2025-03-21 11:21:05 +0530

    fix applied example being treated as a string

--- workspaces/marketplace/plugins/marketplace/src/utils.ts
+++ workspaces/marketplace/plugins/marketplace/src/utils.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright Red Hat, Inc.
+ * Copyright The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,9 +45,9 @@ export const applyContent = (
         );
 
         if (pluginPackage) {
-          const formatContent = newContent
-            .replace(/^```yaml\s+/, '')
-            .replace(/\s+```$/, '');
+          const formatContent = parseDocument(
+            newContent.replace(/^```yaml\s+/, '').replace(/\s+```$/, ''),
+          ).toJS();
           plugin.set('pluginConfig', formatContent);
         }
       }
