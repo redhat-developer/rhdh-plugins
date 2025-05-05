47d77e47b0e32403be06c1c81d6416bdc7663986
Devtools <devtools@redhat.com>
2025-05-05 10:36:39 +0200

    fix custom signin

--- workspaces/sandbox/plugins/sandbox/src/components/SignIn/RHSSOSignInPage.tsx
+++ workspaces/sandbox/plugins/sandbox/src/components/SignIn/RHSSOSignInPage.tsx
@@ -13,68 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { SignInPageProps, useApi } from '@backstage/core-plugin-api';
-import {
-  SignInPage,
-  SignInProviderConfig,
-  Progress,
-} from '@backstage/core-components';
-import { useLocation, useNavigate } from 'react-router-dom';
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { getIdentityResponse } from './helpers';
+import { SignInPageProps } from '@backstage/core-plugin-api';
+import { SignInPage, SignInProviderConfig } from '@backstage/core-components';
+import React from 'react';
 import { keycloakApiRef } from '../../api';
 
 export type Props = SignInPageProps & {
   provider: SignInProviderConfig;
 };
 
-export const RHSSOSignInPage = () => {
-  const authApi = useApi(keycloakApiRef);
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const [isSignedIn, setIsSignedIn] = useState<boolean | null>(null);
-
-  useEffect(() => {
-    const checkSignInStatus = async () => {
-      try {
-        const identityResponse = await getIdentityResponse(authApi);
-        if (!identityResponse) {
-          // Not logged in with SSO
-          setIsSignedIn(false);
-        } else {
-          // They are logged in
-          setIsSignedIn(true);
-        }
-      } catch (err: any) {
-        setIsSignedIn(false);
-        throw err;
-      }
-    };
-    checkSignInStatus();
-  }, [authApi]);
-
-  const handleSignInSuccess = () => {
-    setIsSignedIn(true);
-    navigate(location.pathname);
-    window.location.reload();
-  };
-
-  if (isSignedIn === null) {
-    return <Progress />;
-  }
-
+export const RHSSOSignInPage = (props: Props) => {
   return (
-    <SignInPage
-      auto
-      onSignInSuccess={() => handleSignInSuccess()}
-      provider={{
-        id: 'oidc',
-        title: 'Red Hat SSO',
-        message: 'Sign in using your Red Hat SSO account',
-        apiRef: keycloakApiRef,
-      }}
-    />
+    <div style={{ visibility: 'hidden' }}>
+      <SignInPage
+        {...props}
+        auto
+        provider={{
+          id: 'oidc',
+          title: 'Red Hat SSO',
+          message: 'Sign in using your Red Hat SSO account',
+          apiRef: keycloakApiRef,
+        }}
+      />
+    </div>
   );
 };
