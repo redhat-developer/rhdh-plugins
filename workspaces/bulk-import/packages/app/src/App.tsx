bc4d01dad87ea5d7ce82aec138baa13757d32e69
Yi Cai <yicai@redhat.com>
2025-05-09 16:51:09 -0400

    restore bulk-import updates
    Signed-off-by: Yi Cai <yicai@redhat.com>

--- workspaces/bulk-import/packages/app/src/App.tsx
+++ workspaces/bulk-import/packages/app/src/App.tsx
@@ -47,7 +47,7 @@ import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { UserSettingsPage } from '@backstage/plugin-user-settings';
 import { BulkImportPage } from '@red-hat-developer-hub/backstage-plugin-bulk-import';
-import { getThemes } from '@red-hat-developer-hub/backstage-plugin-theme';
+import { getThemes } from '@redhat-developer/red-hat-developer-hub-theme';
 import { Navigate, Route } from 'react-router-dom';
 import { apis } from './apis';
 import { entityPage } from './components/catalog/EntityPage';
