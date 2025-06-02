23c3c4bc7e81603de27dcd2ada3dab7d6c24ca0d
HusneShabbir <husneshabbir447@gmail.com>
2025-06-02 18:51:47 +0530

    removed visibility statement

--- workspaces/lightspeed/packages/app/e2e-tests/utils/fileUpload.ts
+++ workspaces/lightspeed/packages/app/e2e-tests/utils/fileUpload.ts
@@ -35,9 +35,7 @@ export async function triggerFileChooser(
 }
 
 export async function uploadFile(page: Page, filePath: string) {
-  const attachButton = page.getByRole('button', { name: 'Attach button' });
-  // const attachButton = getElementByRole(page, 'button', 'Attach button');
-  await expect(attachButton).toBeVisible();
+  const attachButton = getElementByRole(page, 'button', 'Attach button');
 
   const fileChooser = await triggerFileChooser(page, attachButton);
   await fileChooser.setFiles(filePath);
