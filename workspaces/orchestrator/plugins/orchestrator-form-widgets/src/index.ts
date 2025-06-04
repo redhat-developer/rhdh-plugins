32b027bc44754ee5e6679c6324deeb7220916fa1
Marek Libra <marek.libra@gmail.com>
2025-06-04 18:50:05 +0200

    Add ClassNameGenerator config for v5 mui

--- workspaces/orchestrator/plugins/orchestrator-form-widgets/src/index.ts
+++ workspaces/orchestrator/plugins/orchestrator-form-widgets/src/index.ts
@@ -13,6 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { unstable_ClassNameGenerator as ClassNameGenerator } from '@mui/material/className';
+
+ClassNameGenerator.configure(componentName => {
+  return componentName.startsWith('v5-')
+    ? componentName
+    : `v5-${componentName}`;
+});
+
 export {
   orchestratorFormWidgetsPlugin,
   orchestratorFormApiFactory,
