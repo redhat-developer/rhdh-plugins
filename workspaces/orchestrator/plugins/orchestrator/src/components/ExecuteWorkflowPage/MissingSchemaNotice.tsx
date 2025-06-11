ce61d0a5cf988b09dba5aa9e35d0b655568f9151
Lior Soffer <liorsoffer1@gmail.com>
2025-06-11 17:27:17 +0300

    fix(orchestrator):remove workflow input editor (#943)
    Signed-off-by: Lior Soffer <liorsoffer1@gmail.com>

---
+++ workspaces/orchestrator/plugins/orchestrator/src/components/ExecuteWorkflowPage/MissingSchemaNotice.tsx
@@ -0,0 +1,58 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+
+import type { JsonObject } from '@backstage/types';
+
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Grid from '@mui/material/Grid';
+
+import { SubmitButton } from '@red-hat-developer-hub/backstage-plugin-orchestrator-form-react';
+
+const MissingSchemaNotice = ({
+  isExecuting,
+  handleExecute,
+}: {
+  isExecuting: boolean;
+  handleExecute: (parameters: JsonObject) => Promise<void>;
+}) => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Alert severity="info" style={{ width: '100%' }}>
+          <AlertTitle>Missing JSON Schema for Input Form</AlertTitle>
+          This workflow will run without any input values.
+          <br />
+          To enable a form-based input, please provide a valid JSON schema in
+          the <code>dataInputSchema</code> property of your workflow definition
+          file.
+        </Alert>
+      </Grid>
+      <Grid item xs={12}>
+        <SubmitButton
+          submitting={isExecuting}
+          handleClick={() => handleExecute({})}
+        >
+          Run
+        </SubmitButton>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default MissingSchemaNotice;
