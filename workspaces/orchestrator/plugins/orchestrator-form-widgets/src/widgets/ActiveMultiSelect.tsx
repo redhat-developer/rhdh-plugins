5b5946d7c89d9f57bc1638819aebda1388df640d
Marek Libra <marek.libra@gmail.com>
2025-06-19 11:48:59 +0200

    fix(orchestrator): fix alignment of ActiveMultiSelect's chips [Backport] (#992)
    * fix(orchestrator): fix alignment of ActiveMultiSelect's chips
    
    Signed-off-by: Marek Libra <marek.libra@gmail.com>
    
    * fix(orchestrator):various fixes for mixed v4 and v5 material UI
    
    Signed-off-by: Marek Libra <marek.libra@gmail.com>
    
    * yarn.lock
    
    ---------
    
    Signed-off-by: Marek Libra <marek.libra@gmail.com>

--- workspaces/orchestrator/plugins/orchestrator-form-widgets/src/widgets/ActiveMultiSelect.tsx
+++ workspaces/orchestrator/plugins/orchestrator-form-widgets/src/widgets/ActiveMultiSelect.tsx
@@ -14,7 +14,10 @@
  * limitations under the License.
  */
 import React, { useEffect, useMemo, useState } from 'react';
+import clsx from 'clsx';
 
+import { makeStyles } from 'tss-react/mui';
+import { Theme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Autocomplete, { AutocompleteValue } from '@mui/material/Autocomplete';
 import FormControl from '@mui/material/FormControl';
@@ -36,11 +39,22 @@ import {
 import { UiProps } from '../uiPropTypes';
 import { ErrorText } from './ErrorText';
 
+const useStyles = makeStyles()(
+  (theme: Theme & { rhdh?: { cardBorderColor: string } }) => ({
+    chip: {
+      // Workaround, we still have mix of Material 4 and 5 CSS in production, conflict with MuiButtonBase-root
+      borderRadius: `16px !important` /* theme.shape.borderRadius is ugly 8px */,
+      outline: `1px solid ${theme.rhdh?.cardBorderColor ?? '#A3A3A3'} !important`,
+    },
+  }),
+);
+
 export const ActiveMultiSelect: Widget<
   JsonObject,
   JSONSchema7,
   OrchestratorFormContextProps
 > = props => {
+  const { classes } = useStyles();
   const templateUnitEvaluator = useTemplateUnitEvaluator();
   const { id, name, label, value: rawValue, onChange, formContext } = props;
   const value = rawValue as string[];
@@ -136,16 +150,20 @@ export const ActiveMultiSelect: Widget<
               />
             )}
             renderTags={(values, getTagProps) =>
-              values.map((item, index) => (
-                <Box key={item} title={item}>
-                  <Chip
-                    data-testid={`${id}-chip-${item}`}
-                    variant="outlined"
-                    label={item}
-                    {...getTagProps({ index })}
-                  />
-                </Box>
-              ))
+              values.map((item, index) => {
+                const tagProps = getTagProps({ index });
+                tagProps.className = clsx(tagProps.className, classes.chip);
+                return (
+                  <Box key={item} title={item}>
+                    <Chip
+                      data-testid={`${id}-chip-${item}`}
+                      variant="outlined"
+                      label={item}
+                      {...tagProps}
+                    />
+                  </Box>
+                );
+              })
             }
           />
         </FormControl>
