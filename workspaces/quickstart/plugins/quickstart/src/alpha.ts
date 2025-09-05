866afbf3a2f1820edffced2ead4180add6772ea7
Lokananda Prabhu <lprabhu@redhat.com>
2025-09-05 15:39:10 +0530

    feat(i18n): add translation support to quickstart plugin with German, French and Spanish translations

--- workspaces/quickstart/plugins/quickstart/src/alpha.ts
+++ workspaces/quickstart/plugins/quickstart/src/alpha.ts
@@ -14,17 +14,4 @@
  * limitations under the License.
  */
 
-import { unstable_ClassNameGenerator as ClassNameGenerator } from '@mui/material/className';
-
-ClassNameGenerator.configure(componentName => {
-  return componentName.startsWith('v5-')
-    ? componentName
-    : `v5-${componentName}`;
-});
-
-export * from './plugin';
-
-export { useQuickstartDrawerContext } from './hooks/useQuickstartDrawerContext';
-export type { QuickstartDrawerContextType } from './components/QuickstartDrawerContext';
-
-export { quickstartTranslations } from './translations';
+export * from './translations';
