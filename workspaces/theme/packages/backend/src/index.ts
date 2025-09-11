37b8b2776c0c00fd1c144e8e7cb871deb3e85704
rhdh-bot service account <rhdh-bot@redhat.com>
2025-09-11 11:47:14 +0200

    theme - version:bump to v1.42.5 (#1415)
    * v1.42.5 version bump
    
    * fix(theme): resolve test-app update issues
    
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>
    
    ---------
    
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Christoph Jerolimov <jerolimov+git@redhat.com>

--- workspaces/theme/packages/backend/src/index.ts
+++ workspaces/theme/packages/backend/src/index.ts
@@ -56,7 +56,7 @@ backend.add(import('@backstage/plugin-search-backend-module-pg/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
-// kubernetes
-backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
+// kubernetes plugin
+backend.add(import('@backstage/plugin-kubernetes-backend'));
 
 backend.start();
