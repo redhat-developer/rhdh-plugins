b8b990b8bb270c13caa80782124c6545e4520d5c
Rodrigo R. Galvão <rgalvao@redhat.com>
2025-11-25 13:28:56 +0100

    fixup! feat(konflux/packages): add app and backend packages for konflux workspace

--- workspaces/konflux/packages/app/src/index.tsx
+++ workspaces/konflux/packages/app/src/index.tsx
@@ -18,4 +18,7 @@ import './global.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<App />);
+}
