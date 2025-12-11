81d1c1b5b495931f4bbcdd2d4ad2e6134896a380
Eswaraiahsapram <esapram@redhat.com>
2025-12-11 07:05:54 +0530

    fix sonarqube issues

--- workspaces/scorecard/plugins/scorecard/src/components/ScorecardHomepageSection/index.ts
+++ workspaces/scorecard/plugins/scorecard/src/components/ScorecardHomepageSection/index.ts
@@ -13,6 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { ScorecardHomepageSection } from './ScorecardHomepageSection';
 
-export { ScorecardHomepageSection };
+export { ScorecardHomepageSection } from './ScorecardHomepageSection';
