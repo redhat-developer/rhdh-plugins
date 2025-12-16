ec8024c7bf8693448c712a7c0db6f11865ddc14e
Jessica He <jhe@redhat.com>
2025-12-16 15:15:56 -0500

    use createOpenSSFMetricProviders
    Signed-off-by: Jessica He <jhe@redhat.com>

--- workspaces/scorecard/plugins/scorecard-backend-module-openssf/src/module.ts
+++ workspaces/scorecard/plugins/scorecard-backend-module-openssf/src/module.ts
@@ -15,24 +15,7 @@
  */
 import { createBackendModule } from '@backstage/backend-plugin-api';
 import { scorecardMetricsExtensionPoint } from '@red-hat-developer-hub/backstage-plugin-scorecard-node';
-import { BinaryArtifactsMetricProvider } from './metricProviders/BinaryArtifactsMetricProvider';
-import { BranchProtectionMetricProvider } from './metricProviders/BranchProtectionMetricProvider';
-import { CITestsMetricProvider } from './metricProviders/CITestsMetricProvider';
-import { CIIBestPracticesMetricProvider } from './metricProviders/CIIBestPracticesMetricProvider';
-import { CodeReviewMetricProvider } from './metricProviders/CodeReviewMetricProvider';
-import { ContributorsMetricProvider } from './metricProviders/ContributorsMetricProvider';
-import { DangerousWorkflowMetricProvider } from './metricProviders/DangerousWorkflowMetricProvider';
-import { DependencyUpdateToolMetricProvider } from './metricProviders/DependencyUpdateToolMetricProvider';
-import { FuzzingMetricProvider } from './metricProviders/FuzzingMetricProvider';
-import { LicenseMetricProvider } from './metricProviders/LicenseMetricProvider';
-import { MaintainedMetricProvider } from './metricProviders/MaintainedMetricProvider';
-import { PackagingMetricProvider } from './metricProviders/PackagingMetricProvider';
-import { PinnedDependenciesMetricProvider } from './metricProviders/PinnedDependenciesMetricProvider';
-import { SASTMetricProvider } from './metricProviders/SASTMetricProvider';
-import { SecurityPolicyMetricProvider } from './metricProviders/SecurityPolicyMetricProvider';
-import { SignedReleasesMetricProvider } from './metricProviders/SignedReleasesMetricProvider';
-import { TokenPermissionsMetricProvider } from './metricProviders/TokenPermissionsMetricProvider';
-import { VulnerabilitiesMetricProvider } from './metricProviders/VulnerabilitiesMetricProvider';
+import { createOpenSSFMetricProviders } from './metricProviders/openSSFMetricProviders';
 
 export const scorecardModuleOpenSSF = createBackendModule({
   pluginId: 'scorecard',
@@ -44,24 +27,7 @@ export const scorecardModuleOpenSSF = createBackendModule({
       },
       async init({ metrics }) {
         // Register all OpenSSF metric providers
-        metrics.addMetricProvider(new BinaryArtifactsMetricProvider());
-        metrics.addMetricProvider(new BranchProtectionMetricProvider());
-        metrics.addMetricProvider(new CITestsMetricProvider());
-        metrics.addMetricProvider(new CIIBestPracticesMetricProvider());
-        metrics.addMetricProvider(new CodeReviewMetricProvider());
-        metrics.addMetricProvider(new ContributorsMetricProvider());
-        metrics.addMetricProvider(new DangerousWorkflowMetricProvider());
-        metrics.addMetricProvider(new DependencyUpdateToolMetricProvider());
-        metrics.addMetricProvider(new FuzzingMetricProvider());
-        metrics.addMetricProvider(new LicenseMetricProvider());
-        metrics.addMetricProvider(new MaintainedMetricProvider());
-        metrics.addMetricProvider(new PackagingMetricProvider());
-        metrics.addMetricProvider(new PinnedDependenciesMetricProvider());
-        metrics.addMetricProvider(new SASTMetricProvider());
-        metrics.addMetricProvider(new SecurityPolicyMetricProvider());
-        metrics.addMetricProvider(new SignedReleasesMetricProvider());
-        metrics.addMetricProvider(new TokenPermissionsMetricProvider());
-        metrics.addMetricProvider(new VulnerabilitiesMetricProvider());
+        metrics.addMetricProvider(...createOpenSSFMetricProviders());
       },
     });
   },
