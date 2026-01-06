c9ef00c9140c57c8161717aab231f4c2a4020888
its-mitesh-kumar <itsmiteshkumar98@gmail.com>
2026-01-06 14:36:12 +0530

    different icons for different sorting
    Signed-off-by: its-mitesh-kumar <itsmiteshkumar98@gmail.com>

--- workspaces/lightspeed/plugins/lightspeed/src/components/LightSpeedChat.tsx
+++ workspaces/lightspeed/plugins/lightspeed/src/components/LightSpeedChat.tsx
@@ -56,6 +56,7 @@ import {
 import {
   PlusIcon,
   SearchIcon,
+  SortAmountDownAltIcon,
   SortAmountDownIcon,
 } from '@patternfly/react-icons';
 import { useQueryClient } from '@tanstack/react-query';
@@ -614,7 +615,12 @@ export const LightspeedChat = ({
               onClick={onSortToggle}
               isExpanded={isSortSelectOpen}
             >
-              <SortAmountDownIcon />
+              {selectedSort === 'oldest' ||
+              selectedSort === 'alphabeticalDesc' ? (
+                <SortAmountDownAltIcon />
+              ) : (
+                <SortAmountDownIcon />
+              )}
             </MenuToggle>
           </Tooltip>
         )}
