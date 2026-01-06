33aae4c833b076845d8b012a54e440dfb23d2b48
Sanket Saikia <sanketsaikia13@gmail.com>
2026-01-06 17:10:20 +0530

    Remove unused import of Navigate from Router.tsx

--- workspaces/bulk-import/plugins/bulk-import/src/components/Router.tsx
+++ workspaces/bulk-import/plugins/bulk-import/src/components/Router.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
