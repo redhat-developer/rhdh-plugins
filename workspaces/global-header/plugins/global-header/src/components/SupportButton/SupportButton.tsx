c624c027d9be865701c85913d79580b8d93af76e
Oskar Jiang <oskarjiang94@gmail.com>
2026-01-23 01:26:07 +0100

    feat(global-header): support multiple support links
    Signed-off-by: Oskar Jiang <oskarjiang94@gmail.com>

--- workspaces/global-header/plugins/global-header/src/components/SupportButton/SupportButton.tsx
+++ workspaces/global-header/plugins/global-header/src/components/SupportButton/SupportButton.tsx
@@ -22,6 +22,17 @@ import { CSSProperties } from 'react';
 import { useTranslation } from '../../hooks/useTranslation';
 import { translateWithFallback } from '../../utils/translationUtils';
 
+interface SupportLink {
+  url: string;
+  title: string;
+}
+
+interface SupportItem {
+  title: string;
+  icon?: string;
+  links: SupportLink[];
+}
+
 /**
  * @public
  */
@@ -30,6 +41,7 @@ export interface SupportButtonProps {
   title?: string;
   titleKey?: string;
   to?: string;
+  items?: SupportItem[];
   tooltip?: string;
   style?: CSSProperties;
   onClick?: () => void;
@@ -41,6 +53,7 @@ export const SupportButton = ({
   title = 'Support',
   titleKey = 'help.supportTitle',
   to,
+  items,
   icon = 'support',
   tooltip,
   style,
@@ -52,6 +65,45 @@ export const SupportButton = ({
 
   const displayTitle = translateWithFallback(t, titleKey, title);
   const supportUrl = to ?? config?.getOptionalString('app.support.url');
+
+  const supportItems =
+    items ??
+    (config
+      ?.getOptionalConfigArray('app.support.items')
+      ?.map(item => ({
+        title: item.getString('title'),
+        icon: item.getOptionalString('icon'),
+        links: item.getConfigArray('links').map(link => ({
+          url: link.getString('url'),
+          title: link.getString('title'),
+        })),
+      })) as SupportItem[] | undefined);
+
+  if (supportItems && supportItems.length > 0) {
+    return (
+      <>
+        {supportItems.map((item: SupportItem) =>
+          item.links.map((link: SupportLink) => (
+            <MenuItem
+              key={link.url}
+              to={link.url}
+              component={Link}
+              sx={{ width: '100%', color: 'inherit', ...style }}
+              onClick={onClick}
+              data-testid="support-button"
+            >
+              <MenuItemLink
+                to={link.url}
+                title={link.title}
+                icon={item.icon ?? icon}
+                tooltip={tooltip}
+              />
+            </MenuItem>
+          )),
+        )}
+      </>
+    );
+  }
 
   if (!supportUrl) {
     return null;
