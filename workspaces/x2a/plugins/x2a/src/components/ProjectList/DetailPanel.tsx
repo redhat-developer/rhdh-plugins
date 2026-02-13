54909f585e1f3db3ebadc9237178e5337e718ee5
Eloy Coto <eloy.coto@acalustra.com>
2026-02-13 16:46:28 +0100

    fix(x2a): added module page CI fixes
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- workspaces/x2a/plugins/x2a/src/components/ProjectList/DetailPanel.tsx
+++ workspaces/x2a/plugins/x2a/src/components/ProjectList/DetailPanel.tsx
@@ -15,9 +15,8 @@
  */
 import { useCallback, useState } from 'react';
 import useAsync from 'react-use/lib/useAsync';
-import { Grid, GridProps, makeStyles, Typography } from '@material-ui/core';
+import { Grid, GridProps, makeStyles } from '@material-ui/core';
 import { Project } from '@red-hat-developer-hub/backstage-plugin-x2a-common';
-import { AboutField } from '@backstage/plugin-catalog';
 
 import { useTranslation } from '../../hooks/useTranslation';
 import { useClientService } from '../../ClientService';
