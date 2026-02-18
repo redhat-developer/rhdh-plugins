7525064461a33b0455e96dec83622436f46abd6d
Christoph Jerolimov <jerolimov+git@redhat.com>
2026-02-18 21:59:03 +0100

    chore(apis): align app-config and playwright config
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>

--- workspaces/apis/playwright.config.ts
+++ workspaces/apis/playwright.config.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The Backstage Authors
+ * Copyright Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,52 +15,42 @@
  */
 
 import { defineConfig } from '@playwright/test';
-import { generateProjects } from '@backstage/e2e-test-utils/playwright';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+const languages = ['en', 'fr', 'it', 'ja'] as const;
+
 export default defineConfig({
-  timeout: 60_000,
+  timeout: 2 * 60 * 1000,
 
   expect: {
-    timeout: 5_000,
+    timeout: 5000,
   },
 
-  // Run your local dev server before starting the tests
-  webServer: process.env.CI
+  webServer: process.env.PLAYWRIGHT_URL
     ? []
-    : [
-        {
-          command: 'yarn start app',
-          port: 3000,
-          reuseExistingServer: true,
-          timeout: 60_000,
-        },
-        {
-          command: 'yarn start backend',
-          port: 7007,
-          reuseExistingServer: true,
-          timeout: 60_000,
-        },
-      ],
-
-  forbidOnly: !!process.env.CI,
+    : {
+        command: 'yarn start',
+        port: 3000,
+        reuseExistingServer: true,
+      },
 
   retries: process.env.CI ? 2 : 0,
 
   reporter: [['html', { open: 'never', outputFolder: 'e2e-test-report' }]],
 
   use: {
-    actionTimeout: 0,
-    baseURL:
-      process.env.PLAYWRIGHT_URL ??
-      (process.env.CI ? 'http://localhost:7007' : 'http://localhost:3000'),
+    baseURL: process.env.PLAYWRIGHT_URL ?? 'http://localhost:3000',
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
   },
 
   outputDir: 'node_modules/.cache/e2e-test-results',
 
-  projects: generateProjects(), // Find all packages with e2e-test folders
+  projects: languages.map(language => ({
+    name: `e2e-tests-${language}`,
+    testDir: 'packages/app/e2e-tests',
+    use: {
+      channel: 'chrome',
+      locale: language,
+    },
+  })),
 });
