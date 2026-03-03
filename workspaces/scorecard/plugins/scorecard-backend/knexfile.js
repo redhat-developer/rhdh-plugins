11b52a5ee379a5ea9ac58f34d6e08ab3c06645a2
Dominika Zemanovicova <dzemanov@redhat.com>
2026-03-03 10:23:06 +0100

    Prefer Number.parseInt
    Signed-off-by: Dominika Zemanovicova <dzemanov@redhat.com>

--- workspaces/scorecard/plugins/scorecard-backend/knexfile.js
+++ workspaces/scorecard/plugins/scorecard-backend/knexfile.js
@@ -42,7 +42,7 @@ module.exports = {
     client: 'pg',
     connection: {
       host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT, 10),
+      port: Number.parseInt(process.env.POSTGRES_PORT, 10),
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
