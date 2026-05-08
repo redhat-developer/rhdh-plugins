82f585ba15d6bec092c16d589076e6deee8b49af
Jordan Dubrick <jdubrick@redhat.com>
2026-05-08 15:04:15 -0400

    update comment
    Signed-off-by: Jordan Dubrick <jdubrick@redhat.com>

--- workspaces/lightspeed/plugins/lightspeed/src/components/LightSpeedChat.tsx
+++ workspaces/lightspeed/plugins/lightspeed/src/components/LightSpeedChat.tsx
@@ -138,6 +138,7 @@ const ConditionalWrapper = ({
 const useStyles = makeStyles(theme => ({
   body: {
     // remove default margin and padding from common elements
+    // lists excluded for proper formatting
     '& h1, & h2, & h3, & h4, & h5, & h6, & p, & li': {
       margin: 0,
       padding: 0,
