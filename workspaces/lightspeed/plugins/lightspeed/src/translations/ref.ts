d2fc9763eaf5d45a9c341701ef0884a082f7fa73
Lucas <lyoon@redhat.com>
2026-05-18 21:07:13 -0400

    address comments
    Signed-off-by: Lucas <lyoon@redhat.com>

--- workspaces/lightspeed/plugins/lightspeed/src/translations/ref.ts
+++ workspaces/lightspeed/plugins/lightspeed/src/translations/ref.ts
@@ -78,8 +78,7 @@ export const lightspeedMessages = {
     'Maximum 10 documents are allowed. Delete a document to upload a new document.',
   'notebook.view.documents.uploadsInProgress':
     'Please wait for current uploads to complete before adding more documents.',
-  'notebook.upload.success': '{{fileName}} Successfully Uploaded.',
-  'notebook.upload.failed': '{{fileName}} Upload Failed.',
+  'notebook.upload.failed': '"{{fileName}}" upload failed.',
 
   // Notebook upload modal
   'notebook.upload.modal.title': 'Add a document to Notebook',
