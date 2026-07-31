26c533ba566ca903c3c7beea9faec9c5792b9c33
Michael Valdron <mvaldron@redhat.com>
2026-07-31 12:28:57 -0400

    yarn prettier -w plugins/intelligent-assistant-backend/src/service/router.ts
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- workspaces/intelligent-assistant/plugins/intelligent-assistant-backend/src/service/router.ts
+++ workspaces/intelligent-assistant/plugins/intelligent-assistant-backend/src/service/router.ts
@@ -28,13 +28,13 @@ import express, { Router } from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 import {
-  iaSavedPromptsManagePermission,
   iaChatAccessPermission,
   iaChatManagePermission,
   iaChatUsePermission,
   iaMcpManagePermission,
   iaMcpReadPermission,
   iaPermissions,
+  iaSavedPromptsManagePermission,
 } from '@red-hat-developer-hub/backstage-plugin-intelligent-assistant-common';
 
 import { Readable } from 'node:stream';
