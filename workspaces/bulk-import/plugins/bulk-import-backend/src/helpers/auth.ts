e03ad4723e661c10c4ea7c4bc57526380a77bf0c
Debsmita Santra <debsmita.santra@gmail.com>
2026-09-10 19:05:16 +0530

    feat(bulk-import): add if predicate gating
    Signed-off-by: Debsmita Santra <debsmita.santra@gmail.com>

--- workspaces/bulk-import/plugins/bulk-import-backend/src/helpers/auth.ts
+++ workspaces/bulk-import/plugins/bulk-import-backend/src/helpers/auth.ts
@@ -43,7 +43,6 @@ export async function permissionCheck(
       [
         {
           permission: bulkImportPermission,
-          resourceRef: bulkImportPermission.resourceType,
         },
       ],
       {
